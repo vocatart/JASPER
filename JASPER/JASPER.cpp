c487f76b1e02cad46790c3cae7f63feba29a317d
tart <conemusicproductions@gmail.com>
2021-06-08 22:21:58 -0600

    begin writing main function

---
+++ JASPER/JASPER.cpp
@@ -0,0 +1,28 @@
+// UTAU Synthesis Engine JASPER
+// Version v1.0.0
+// Powered by libllsm by Kanru Hua
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+// - 13 Arguments -
+// 1 - Input File
+// 2 - Output File
+// 3 - Pitch
+// 4 - Consonant Velocity
+// 5 - Flags
+// 6 - Offset (Left Blank)
+// 7 - Note Length
+// 8 - Consonant Length (Consonant)
+// 9 - Cuttoff (Right Blank)
+// 10 - Volume (Intensity)
+// 11 - Modulation
+// 12 - Tempo
+// 13 - Pitchbend
+
+
+// Main
+int main(int argc, char *argv[]) {
+
+}
