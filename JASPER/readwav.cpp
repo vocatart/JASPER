dbcf7306f1be1287c7fdc49e508f9d0ff4619bfc
tart <conemusicproductions@gmail.com>
2021-06-09 12:36:11 -0600

    Update readwav.cpp

--- JASPER/readwav.cpp
+++ JASPER/readwav.cpp
@@ -45,7 +45,11 @@ double* ReadWavFile(
 	// Catch file read error
 	if (NULL == file)
 	{
-		fprintf(stderr, "Error: Cannot read file!!!\n");
+		fclose(file);
+		system("Color 0B");
+		fprintf(stderr, "Error: ");
+		system("Color 07");
+		fprintf(stderr, "Cannot read file!!!\n");
 		return NULL;
 	}
 
@@ -55,7 +59,10 @@ double* ReadWavFile(
 	if (0 != strcmp(header_buf, "RIFF"))
 	{
 		fclose(file);
-		fprintf(stderr, "Error: RIFF Header Missing!!!\n");
+		system("Color 0B");
+		fprintf(stderr, "Error: ");
+		system("Color 07");
+		fprintf(stderr, "RIFF Header Missing!!!\n");
 		return NULL;
 	}
 
@@ -65,7 +72,10 @@ double* ReadWavFile(
 	if (0 != strcmp(header_buf, "WAVE"))
 	{
 		fclose(file);
-		fprintf(stderr, "Error: WAVE Header Missing!!!\n");
+		system("Color 0B");
+		fprintf(stderr, "Error: ");
+		system("Color 07");
+		fprintf(stderr, "WAVE Header Missing!!!\n");
 		return NULL;
 	}
 
@@ -74,7 +84,10 @@ double* ReadWavFile(
 	if (0 != strcat(header_buf, "fmt "))
 	{
 		fclose(file);
-		fprintf(stderr, "Error: fmt Header Missing!!!\n");
+		system("Color 0B");
+		fprintf(stderr, "Error: ");
+		system("Color 07");
+		fprintf(stderr, "fmt Header Missing!!!\n");
 		return NULL;
 	}
 
@@ -83,7 +96,10 @@ double* ReadWavFile(
 	if (!(16 == header_buf[0] && 0 == header_buf[1] && 0 == header_buf[2] && 0 == header_buf[3]))
 	{
 		fclose(file);
-		fprintf(stderr, "Error: Incorrect or Missing fmt length!!!\n");
+		system("Color 0B");
+		fprintf(stderr, "Error: ");
+		system("Color 07");
+		fprintf(stderr, "Incorrect or Missing fmt length!!!\n");
 		return NULL;
 	}
 
@@ -92,6 +108,71 @@ double* ReadWavFile(
 	if (!(1 == header_buf[0] && 0 == header_buf[1]))
 	{
 		fclose(file);
-		fprintf(stderr, "Error: Missing format ID!!!\n");
+		system("Color 0B");
+		fprintf(stderr, "Error: ");
+		system("Color 07");
+		fprintf(stderr, "Missing format ID!!!\n");
 	}
+
+	// Check for more than one channel
+	// JASPER will only use the first channel
+	read_result = fread(integer_buf, sizeof(char), 2, file);
+	assert(read_result == 2);
+	num_channels = integer_buf[0];
+	if (num_channels > 1)
+	{
+		system("Color 06");
+		fprintf(stderr, "Warning: ");
+		system("Color 07");
+		fprintf(stderr, " Multiple channels detected. JASPER will only use the first channel.");
+	}
+
+	// Sampling Frequency (Hz)
+	read_result = fread(integer_buf, sizeof(char), 4, file);
+	assert(read_result == 4);
+	*samplerate = 0;
+	for (int i = 3; i >= 0; i--)
+	{
+		*samplerate = (*samplerate * 256) + integer_buf[i];
+	}
+
+	// Bits per Sample
+	fseek(file, 6, SEEK_CUR); // Skip Bytes per Second and Bytes per Sample
+	read_result = fread(integer_buf, sizeof(char), 2, file);
+	assert(read_result == 2);
+	*bitsPerSample = integer_buf[0];
+	bytesPerSample = *bitsPerSample / 8; // Get Bits/Sample
+	if (*bitsPerSample % 8 > 0) bytesPerSample++; // Rounding up, if needed
+
+	// Find required Data Header
+	int chunkLength;
+	read_result = fread(header_buf, sizeof(char), 4, file);
+	assert(read_result == 4);
+	while (strcmp(header_buf, "data") != 0)
+	{
+		read_result = fread(&chunkLength, sizeof(char), 4, file);
+		assert(read_result == 4);
+		fseek(file, chunkLength, SEEK_CUR); // Unimportant Chunk
+		read_result = fread(header_buf, sizeof(char), 4, file);
+		assert(read_result == 4);
+	}
+
+	// # of Samples
+	read_result = fread(integer_buf, sizeof(char), 4, file);
+	assert(read_result == 4);
+	*numSample = 0;
+	for (int i = 3; i >= 0; i--)
+	{
+		*numSample = (*numSample * 256) + integer_buf[i];
+	}
+	*numSample /= (bytesPerSample * num_channels); // Divide by bytes/sample
+
+	if (*cuttoff_ms < 0) // If cuttoff is negative, add to offset instead of subtracting from the end
+	{
+		*cuttoff_ms = (*numSample * 1000.0 / *samplerate) - (*offset_ms - *cuttoff_ms);
+	}
+
+	int start, end;
+
+
 }
